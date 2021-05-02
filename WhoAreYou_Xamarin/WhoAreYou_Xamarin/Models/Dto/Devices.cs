3b32402cfe199de25b33484f75e87bb785c52194
joebrooks <lcw3176@naver.com>
2021-05-03 00:21:34 +0900

    삭제 커맨드 추가

--- WhoAreYou_Xamarin/WhoAreYou_Xamarin/Models/Dto/Devices.cs
+++ WhoAreYou_Xamarin/WhoAreYou_Xamarin/Models/Dto/Devices.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace WhoAreYou_Xamarin.Models
 {
@@ -8,5 +7,6 @@ namespace WhoAreYou_Xamarin.Models
         public int index { get; set; }
         public string name { get; set; }
         public ICommand searchLogCommand { get; set; }
+        public ICommand deleteCommand { get; set; }
     }
 }
