9955d0c4f0f01a63494a3d8baebb1ad5901888f9
joebrooks <lcw3176@naver.com>
2021-05-22 10:39:00 +0900

    메세지 서비스 추가

---
+++ WhoAreYou_Xamarin/WhoAreYou_Xamarin/Services/MessageService.cs
@@ -0,0 +1,13 @@
+﻿using WhoAreYou_Xamarin.Services.Dependencies;
+using Xamarin.Forms;
+
+namespace WhoAreYou_Xamarin.Services
+{
+    public static class MessageService
+    {
+        public static void Show(string msg)
+        {
+            DependencyService.Get<DIToastMessage>().Alert(msg);
+        }
+    }
+}
