c2e3f7259b0b0e21bc06798e0ca25b5c8470ba2a
joebrooks <lcw3176@naver.com>
2021-05-31 09:43:31 +0900

    블루투스 커맨드 추가

---
+++ WhoAreYou_Xamarin/WhoAreYou_Xamarin/Models/Enum/BLECommand.cs
@@ -0,0 +1,12 @@
+﻿namespace WhoAreYou_Xamarin.Models.Enum
+{
+    public enum BLECommand
+    {
+        SetWiFiName,
+        SetWiFiPassword,
+        DisconnectWiFi,
+        SetDeviceName,
+        SetEmail,
+        SetToken,
+    }
+}
